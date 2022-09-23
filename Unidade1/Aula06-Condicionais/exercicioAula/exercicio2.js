c1ca4bd16729f1cdd1b9d623395d020a2522833a
Leonardo Reis <96137175+LeonardoReis86@users.noreply.github.com>
2022-09-23 16:19:52 -0300

    update exercicio2 de aula

--- Unidade1/Aula06-Condicionais/exercicioAula/exercicio2.js
+++ Unidade1/Aula06-Condicionais/exercicioAula/exercicio2.js
@@ -6,7 +6,18 @@
 // n = tempo
 
 let valorDaCompra = 100;
+let numeroDeParcelas = 12;
 const taxaDeJuros = 1 / 100;
-const montante = valorDaCompra * Math.pow(1 + taxaDeJuros, 12);
+const montante = valorDaCompra * Math.pow(1 + taxaDeJuros, numeroDeParcelas);
 const valorAPagar = montante.toFixed(2);
-console.log(valorAPagar);
+
+if (numeroDeParcelas === 1) {
+  let valorAPagar = valorDaCompra - desconto;
+  const desconto = 0.1 * valorDaCompra;
+  console.log(valorAPagar.toFixed(2));
+} else if (numeroDeParcelas > 1 && numeroDeParcelas <= 6) {
+  let valorAPagar = valorDaCompra / numeroDeParcelas;
+  console.log(valorAPagar.toFixed(2));
+} else if (numeroDeParcelas > 6 && numeroDeParcelas < 13) {
+  console.log(montante.toFixed(2));
+}
