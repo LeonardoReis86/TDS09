459c799fac7b946dae939e39dda8db1a8c7c1f96
Leonardo Reis <96137175+LeonardoReis86@users.noreply.github.com>
2022-09-23 11:48:24 -0300

    update exercicio2.js

--- Unidade1/Aula06-Condicionais/exercicioAula/exercicio2.js
+++ Unidade1/Aula06-Condicionais/exercicioAula/exercicio2.js
@@ -1,3 +1,12 @@
 //Aplicar taxa dde juros de 1% a.m.
 //Parcelar em até 12x
 //Valor a pagar M = C (1+i)ˆn
+//M = montante a pagar
+// i = taxa de juros
+// n = tempo
+
+let valorDaCompra = 100;
+const taxaDeJuros = 1 / 100;
+const montante = valorDaCompra * Math.pow(1 + taxaDeJuros, 12);
+const valorAPagar = montante.toFixed(2);
+console.log(valorAPagar);
