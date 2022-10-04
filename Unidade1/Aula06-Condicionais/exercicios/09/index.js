bc9266bb72fbf1cfc5242eb95dd95bb71d0e7fe7
Leonardo Reis <96137175+LeonardoReis86@users.noreply.github.com>
2022-10-04 10:32:31 -0300

    Resolucao exercicio 9

---
+++ Unidade1/Aula06-Condicionais/exercicios/09/index.js
@@ -0,0 +1,13 @@
+const nota = 8.5;
+
+if (nota > 8.9) {
+  console.log("O estudante obteve conceito A");
+} else if (nota > 7.9) {
+  console.log("O estudante obteve conceito B");
+} else if (nota > 5.9) {
+  console.log("O estudante obteve conceito C");
+} else if (nota > 4) {
+  console.log("O estudante obteve cenceito D");
+} else {
+  console.log("O estudante obteve conceito E");
+}
